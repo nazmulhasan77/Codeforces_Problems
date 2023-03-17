587319aeebd3e66655c50c197335750624dfec2b
nazmulhasan77 <namzul7762@gmail.com>
2023-03-17 20:43:15 +0600

    Sum Added

--- Hello.c
+++ Hello.c
@@ -4,7 +4,12 @@ int main()
         printf("Hello World");
 	for(int i=0 ; i<5 ; i++){
 		printf("Hello");
+
 	}
+	//New Brance Added Sum
+	int a=5,b=10;
+	int sum = a+b;
+	printf("%d",sum);
         return 0;
 }
 
